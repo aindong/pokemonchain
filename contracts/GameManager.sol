1ebc08c045d0ab5dbc06a42df78ec27777ad9769
Alleo Indong <freedom_war550@live.com.ph>
2021-12-13 20:06:27 +0800

    initial game contract and test runner

---
+++ contracts/GameManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract GameManager {
+    constructor() {
+        console.log("GameManager: Initial game contract yay!");
+    }
+
+    
+}
---
+++ scripts/run.js
@@ -0,0 +1,21 @@
+const main = async () => {
+  const gameManagerContractFactory = await hre.ethers.getContractFactory(
+    "GameManager"
+  );
+  const gameContract = await hre.ethers.deploy();
+  await gameContract.deployed();
+
+  console.log("Game deployed at:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+runMain();
